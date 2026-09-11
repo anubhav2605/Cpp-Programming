ce6feb9f630a2094b8480e11299af6a6d4f904e3
Anubhav verma <anubhavv723@gmail.com>
2026-09-11 23:14:23 +0530

    Create 36_MatrixAddition.cpp

---
+++ 36_MatrixAddition.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matrix1[2][2] = {
+        {1, 2},
+        {3, 4}
+    };
+
+    int matrix2[2][2] = {
+        {5, 6},
+        {7, 8}
+    };
+
+    int sum[2][2];
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            sum[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    cout << "Sum of matrices:" << endl;
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
