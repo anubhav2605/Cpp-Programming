4afdc53261443dee844f36335f8e6b6bdbac27b2
Anubhav verma <anubhavv723@gmail.com>
2026-09-11 23:02:14 +0530

    Create 27_SwapNumbers.cpp

---
+++ 27_SwapNumbers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, temp;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    cout << "Before swapping: " << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "After swapping: " << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    return 0;
+}
