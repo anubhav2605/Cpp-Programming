db0ef5b54f4b2af35b78a0c2affb585943002ab6
Anubhav verma <anubhavv723@gmail.com>
2026-09-11 22:33:53 +0530

    Create 06_GradeCalculator.cpp

---
+++ 06_GradeCalculator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks;
+
+    cout << "Enter marks (0-100): ";
+    cin >> marks;
+
+    if (marks >= 90 && marks <= 100) {
+        cout << "Grade A";
+    }
+    else if (marks >= 75) {
+        cout << "Grade B";
+    }
+    else if (marks >= 50) {
+        cout << "Grade C";
+    }
+    else if (marks >= 0) {
+        cout << "Fail";
+    }
+    else {
+        cout << "Invalid marks";
+    }
+
+    return 0;
+}
