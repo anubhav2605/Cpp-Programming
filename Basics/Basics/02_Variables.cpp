49f7fe6f059bc49e4a914125b67e2c14074cabc2
Anubhav verma <anubhavv723@gmail.com>
2026-09-11 22:10:24 +0530

    Create 02_Variables.cpp

---
+++ Basics/Basics/02_Variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name = "Anubhav";
+    int age = 18;
+    float marks = 85.5f;
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Marks: " << marks << endl;
+
+    return 0;
+}
