9f42d318b5f74337ad16470f1f9d209ba2057f45
Anubhav verma <anubhavv723@gmail.com>
2026-09-11 22:52:48 +0530

    Create 19_String.cpp

---
+++ 19_String.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+
+    cout << "Enter your full name: ";
+    getline(cin, name);
+
+    cout << "Your name is: " << name << endl;
+
+    return 0;
+}
