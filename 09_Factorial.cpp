d9bfaf0c18b030601b3d0e11c8536c3627809384
Anubhav verma <anubhavv723@gmail.com>
2026-09-11 22:37:18 +0530

    Create 09_Factorial.cpp

---
+++ 09_Factorial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    long long factorial = 1;
+
+    cout << "Enter a non-negative integer: ";
+    cin >> num;
+
+    if (num < 0) {
+        cout << "Factorial is not defined for negative numbers.";
+    } else {
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+
+        cout << "Factorial of " << num << " = " << factorial;
+    }
+
+    return 0;
+}
